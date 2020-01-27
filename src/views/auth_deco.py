6d22d313ee57ace828789a1ac05f7da99a46b063
lmdslyngl <lmdslyngl0315@gmail.com>
2020-01-27 20:28:27 +0900

    CSRFトークンを必須とするデコレータを作成した

--- src/views/auth_deco.py
+++ src/views/auth_deco.py
@@ -1,7 +1,7 @@
 
 import functools
 import flask
-from .util import get_logined_user
+from .util import get_logined_user, get_csrf_token
 
 
 def login_required(func):
@@ -24,3 +24,26 @@ def logout_required(func):
                 error_message="ログインしている状態では使用できないページです。")
         return func(*args, **kwargs)
     return wrapper
+
+
+def csrf_token_required(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        token = get_csrf_token()
+
+        if "csrf-token" in flask.request.form:
+            token_in_request = flask.request.form["csrf-token"]
+        else:
+            return flask.render_template(
+                "error.html",
+                error_message="CSRFトークンが見つかりません。")
+
+        if token_in_request != token:
+            return flask.render_template(
+                "error.html",
+                error_message="CSRFトークンが一致しません。")
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
