f5365031cae77fce2950235feeef1c2b75bc2db1
lmdslyngl <lmdslyngl0315@gmail.com>
2020-02-02 15:45:17 +0900

    get_url関数でクエリ引数なしのURLを取得できるようにした

--- src/views/template_functions.py
+++ src/views/template_functions.py
@@ -1,10 +1,15 @@
 
+from urllib.parse import urlparse
 import flask
 from .util import get_logined_user, get_csrf_token
 
 
-def get_url() -> str:
-    return flask.request.url
+def get_url(with_args=True) -> str:
+    parsed_url = urlparse(flask.request.url)
+    if with_args:
+        return parsed_url.path + parsed_url.query
+    else:
+        return parsed_url.path
 
 
 def register_template_functions(app: flask.Flask) -> None:
