9a6ffe0238d67d62f9e053ee7d215d665630a184
lmdslyngl <lmdslyngl0315@gmail.com>
2020-02-09 15:10:11 +0900

    1ページあたりの表示数を20にした

--- src/conf.py
+++ src/conf.py
@@ -7,7 +7,7 @@ db_name = "bbsdb2"
 db_max_pool_connections = 20
 
 # 掲示板一覧の1ページあたりの掲示板数
-boardlist_boards_per_page = 2
+boardlist_boards_per_page = 20
 
 # 掲示板の1ページあたりの投稿数
-board_posts_per_page = 2
+board_posts_per_page = 20
